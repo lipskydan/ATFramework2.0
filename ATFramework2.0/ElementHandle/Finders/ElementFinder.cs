2d15e0f3b39c8c9418cc9fcb02b055d00c35308a
Danial Lipsky <43038176+lipskydan@users.noreply.github.com>
2024-03-12 23:16:17 +0200

    update ElementFinder logic

--- ATFramework2.0/ElementHandle/Finders/ElementFinder.cs
+++ ATFramework2.0/ElementHandle/Finders/ElementFinder.cs
@@ -4,15 +4,13 @@ public class ElementFinder
 {
     private readonly IWebDriverManager _driverManager;
 
-    public ElementFinder(IWebDriverManager driverManager)
-    {
-        _driverManager = driverManager;
-    }
+    public ElementFinder(IWebDriverManager driverManager) => _driverManager = driverManager;
 
-    public Element Css(string cssSelector) => new(_driverManager.FindElement(By.CssSelector(cssSelector)));
-    public Element XPath(string xpath) => new(_driverManager.FindElement(By.XPath(xpath)));
-    public Element Id(string id) => new(_driverManager.FindElement(By.Id(id)));
-    public Element LinkText(string text) => new(_driverManager.FindElement(By.LinkText(text)));
-    
-    public IWebElement _Id(string id) => _driverManager.FindElement(By.Id(id));
+    public Element Css(string cssSelector) => new(_driverManager.WebDriverWait.Value.Until(_ => _driverManager.Driver.FindElement(By.CssSelector(cssSelector))));
+    public Element XPath(string xpath) => new(_driverManager.WebDriverWait.Value.Until(_ => _driverManager.Driver.FindElement(By.XPath(xpath))));
+    public Element Id(string id) => new(_driverManager.WebDriverWait.Value.Until(_ => _driverManager.Driver.FindElement(By.Id(id))));
+
+    //public Element LinkText(string text) => new(_driverManager.FindElement(By.LinkText(text)));
+    //TODO: fix _Id (now use for SelectElement)
+    public IWebElement _Id(string id) => _driverManager.WebDriverWait.Value.Until(_ => _driverManager.Driver.FindElement(By.Id(id)));
 }
