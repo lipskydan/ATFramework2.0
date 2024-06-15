745f0c61799ccb6942a88ab49e3b1d31f4b4bfa9
Danial Lipsky <43038176+lipskydan@users.noreply.github.com>
2024-06-15 23:10:52 +0300

    add tests for local storage worker and update LS

--- ATFramework2.0/Utilities/LocalStorageWorker.cs
+++ ATFramework2.0/Utilities/LocalStorageWorker.cs
@@ -2,19 +2,34 @@
 
 public class LocalStorageWorker
 {
-    private IWebDriverManager _driverManger;
     private IJavaScriptExecutor js;
 
-    public LocalStorageWorker(IWebDriverManager driverManger)
+    public LocalStorageWorker(IWebDriver driver)
     {
-        _driverManger = driverManger;
-        js = (IJavaScriptExecutor)_driverManger.Driver;
+        js = (IJavaScriptExecutor)driver;
     }
 
     public Dictionary<string, string> GetLocalStorage()
     {   
-        return (Dictionary<string, string>)js.ExecuteScript("return window.localStorage;");
+        var localStorage = (Dictionary<string, object>)js.ExecuteScript(@"
+            let items = {}; 
+            for (let i = 0; i < localStorage.length; i++) {
+                let key = localStorage.key(i);
+                items[key] = localStorage.getItem(key);
+            }
+            return items;
+        ");
+
+        // Convert Dictionary<string, object> to Dictionary<string, string>
+        var result = new Dictionary<string, string>();
+        foreach (var kvp in localStorage)
+        {
+            result.Add(kvp.Key, kvp.Value.ToString());
+        }
+
+        return result;
     }
+
     public void AddToLocalStorage(string key, string value)
     {
         js.ExecuteScript($"window.localStorage.setItem('{key}', '{value}');");
@@ -26,5 +41,9 @@ public class LocalStorageWorker
     public void DeleteFromLocalStorage(string key)
     {
         js.ExecuteScript($"window.localStorage.removeItem('{key}');");
+    }
+     public void ClearLocalStorage()
+    {
+        js.ExecuteScript("window.localStorage.clear();");
     }
 }
